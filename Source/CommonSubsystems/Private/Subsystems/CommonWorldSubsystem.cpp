e3196d2a43dced021ba3307d8b908f7d22f78ff8
Tonetfal <antonio.sid99@gmail.com>
2025-06-04 12:20:08 +0200

    improve world subsystem

--- Source/CommonSubsystems/Private/Subsystems/CommonWorldSubsystem.cpp
+++ Source/CommonSubsystems/Private/Subsystems/CommonWorldSubsystem.cpp
@@ -1,3 +1,265 @@
 ﻿// Author: Antonio Sidenko (Tonetfal), July 2023
 
 #include "Subsystems/CommonWorldSubsystem.h"
+
+#include "Editor.h"
+#include "Editor/EditorEngine.h"
+#include "Engine/NetDriver.h"
+#include "GameMapsSettings.h"
+#include "LogCategories.h"
+
+DEFINE_LOG_CATEGORY(LogCommonSubsystems);
+
+static uint8 GetNetModeInteger(ECommonNetMode NetMode)
+{
+	const uint8 NetModeInteger = static_cast<uint8>(1) << static_cast<uint8>(NetMode);
+	return NetModeInteger;
+}
+
+UCommonWorldSubsystem::UCommonWorldSubsystem()
+{
+	// Skip 'Entry' and 'MainMenu' levels by default
+	LevelBlocklist.Add("L_Entry");
+	LevelBlocklist.Add("L_MainMenu");
+
+	bEnableInUntitledLevel = false;
+	bEnableInTransitionLevel = false;
+
+	// Initialise in all network domains by default
+	ClearSupportedNetMode();
+	AddSupportedNetMode(ECommonNetMode::Standalone);
+	AddSupportedNetMode(ECommonNetMode::DedicatedServer);
+	AddSupportedNetMode(ECommonNetMode::ListenServer);
+	AddSupportedNetMode(ECommonNetMode::Client);
+}
+
+void UCommonWorldSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	Tick_Initialize(FTickSignature::CreateUObject(this, &ThisClass::Tick));
+
+	// We can't do safe initialization until much later
+	PostInitWorldDelegateHandle = FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(
+		this, &ThisClass::PostInitWorldInternal);
+
+#if WITH_EDITOR
+	// Editor environment is added
+	PostInitPieWorldDelegateHandle = FEditorDelegates::PostPIEStarted.AddUObject(
+		this, &ThisClass::PostInitPieWorldInternal);
+#endif
+}
+
+void UCommonWorldSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+
+	Tick_Deinitialize();
+}
+
+bool UCommonWorldSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!Super::ShouldCreateSubsystem(Outer))
+	{
+		return false;
+	}
+
+	const auto* World = CastChecked<UWorld>(Outer);
+	if (!World->IsGameWorld())
+	{
+		return false;
+	}
+
+#if WITH_EDITOR
+	// PIE creates temporary worlds for Net Clients which should really never initialize subsystems
+	if (World->IsPlayInEditor())
+	{
+		const UPackage* Package = CastChecked<UPackage>(World->GetOuter());
+		const int32 PieInstanceId = Package ? Package->GetPIEInstanceID() : INDEX_NONE;
+		if (PieInstanceId == INDEX_NONE)
+		{
+			return false;
+		}
+	}
+#endif
+
+	if (!CheckNetMode(World))
+	{
+		ENetMode MyNetMode = NM_MAX;
+		GetSafeNetMode(OUT MyNetMode, World);
+
+		UE_LOG(LogCommonSubsystems, Log, TEXT("Subsystem [%s] has not been created because net mode [%s] is not "
+			"supported."), *GetName(), *UEnum::GetValueAsString(static_cast<ECommonNetMode>(MyNetMode)));
+
+		return false;
+	}
+
+	if (!CheckLevelName(World))
+	{
+		UE_LOG(LogCommonSubsystems, Log, TEXT("Subsystem [%s] has not been created because level [%s] is not "
+			"supported."), *GetName(), *World->GetMapName());
+
+		return false;
+	}
+
+	return true;
+}
+
+bool UCommonWorldSubsystem::DoesSupportWorldType(const EWorldType::Type WorldType) const
+{
+	return Super::DoesSupportWorldType(WorldType);
+}
+
+void UCommonWorldSubsystem::Tick(float DeltaSeconds)
+{
+	// Empty
+}
+
+bool UCommonWorldSubsystem::IsNetModeSupported(ECommonNetMode NetMode) const
+{
+	const bool bIsNetModeSupported = InitializationNetModeMask | GetNetModeInteger(NetMode);
+	return bIsNetModeSupported;
+}
+
+bool UCommonWorldSubsystem::GetSafeNetMode(ENetMode& OutNetMode, const UWorld* OverrideWorld /*nullptr*/) const
+{
+	const UWorld* World = OverrideWorld ? OverrideWorld : GetWorld();
+	check(World);
+
+	OutNetMode = World->GetNetMode();
+
+#if WITH_EDITOR
+	// PIE creates some obscure UWorld lifetimes that make NetMode access more difficult
+	// See https://udn.unrealengine.com/s/question/0D54z00007DW1CQCA1/why-are-uworldsubsystems-initialized-before-uworld-is-initialized
+	if (!World->IsPlayInEditor())
+	{
+		return true;
+	}
+
+	UObject* Outer = World->GetOuter();
+	const auto* Package = Cast<UPackage>(Outer);
+	const int32 PieInstanceId = Package ? Package->GetPIEInstanceID() : INDEX_NONE;
+	if (PieInstanceId == INDEX_NONE)
+	{
+		// This is a Transient UPackage used for PIE Client worlds
+		OutNetMode = NM_MAX;
+		return false;
+	}
+
+	// We can get the UGameInstance/UNetDriver from here, *before* the UWorld properties are set
+	const auto* EditorEngine = CastChecked<UEditorEngine>(GEngine);
+	const FWorldContext* PieWorldContext = EditorEngine->GetWorldContextFromPIEInstance(PieInstanceId);
+	if (ensureAlways(PieWorldContext))
+	{
+		if (!PieWorldContext->PendingNetGame)
+		{
+			OutNetMode = PieWorldContext->RunAsDedicated ? NM_DedicatedServer : NM_Standalone;
+		}
+		else if (ensureAlways(PieWorldContext->PendingNetGame->NetDriver))
+		{
+			OutNetMode = PieWorldContext->PendingNetGame->NetDriver->GetNetMode();
+		}
+	}
+#endif
+
+	return true;
+}
+
+FTimerManager& UCommonWorldSubsystem::GetTimerManager() const
+{
+	return GetWorld()->GetTimerManager();
+}
+
+void UCommonWorldSubsystem::OnWorldInitialized()
+{
+	// Empty
+}
+
+void UCommonWorldSubsystem::AddSupportedNetMode(ECommonNetMode NetMode)
+{
+	InitializationNetModeMask |= GetNetModeInteger(NetMode);
+}
+
+void UCommonWorldSubsystem::RemoveSupportedNetMode(ECommonNetMode NetMode)
+{
+	InitializationNetModeMask ^= GetNetModeInteger(NetMode);
+}
+
+void UCommonWorldSubsystem::ClearSupportedNetMode()
+{
+	InitializationNetModeMask = 0;
+}
+
+bool UCommonWorldSubsystem::CheckNetMode(const UWorld* World) const
+{
+	ENetMode MyNetMode = NM_MAX;
+	if (GetSafeNetMode(OUT MyNetMode, World))
+	{
+		const uint8 NetModeMask = GetNetModeInteger(static_cast<ECommonNetMode>(MyNetMode));
+		const uint8 MatchingModes = InitializationNetModeMask & NetModeMask;
+		const bool bMatchesAnything = MatchingModes != 0;
+		return bMatchesAnything;
+	}
+
+	return false;
+}
+
+bool UCommonWorldSubsystem::CheckLevelName(const UWorld* World) const
+{
+	checkSlow(World);
+
+	// Check Level Names
+	FString LevelName = World->GetMapName();
+	LevelName.RemoveFromStart(World->StreamingLevelsPrefix);
+
+	// Optionally Block in the 'Untitled' Level
+	// These are special levels the engine sometimes creates for intermediate UWorlds.
+	if (!bEnableInUntitledLevel && LevelName.Contains(TEXT("Untitled")))
+	{
+		return false;
+	}
+
+	// Optionally Block in the 'Transition' Level
+	// Read the Transition Level from project settings.
+	const auto* MapSettings = GetDefault<UGameMapsSettings>();
+	check(MapSettings);
+
+	if (!bEnableInTransitionLevel && !MapSettings->TransitionMap.IsNull() &&
+		MapSettings->TransitionMap.GetAssetName() == LevelName)
+	{
+		return false;
+	}
+
+	if (!LevelAllowlist.IsEmpty() && !LevelAllowlist.Contains(LevelName))
+	{
+		return false;
+	}
+
+	if (LevelBlocklist.Contains(LevelName))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void UCommonWorldSubsystem::PostInitWorldInternal(UWorld* NewWorld)
+{
+	const UWorld* World = GetWorld();
+	if (IsValid(World) && World == NewWorld && !bHasPostWorldInitialized)
+	{
+		bHasPostWorldInitialized = true;
+
+		FCoreUObjectDelegates::PostLoadMapWithWorld.Remove(PostInitWorldDelegateHandle);
+#if WITH_EDITOR
+		FEditorDelegates::PostPIEStarted.Remove(PostInitPieWorldDelegateHandle);
+#endif
+
+		OnWorldInitialized();
+	}
+}
+
+void UCommonWorldSubsystem::PostInitPieWorldInternal(bool bSimulating)
+{
+	PostInitWorldInternal(GetWorld());
+}
--- Source/CommonSubsystems/Public/Subsystems/CommonWorldSubsystem.h
+++ Source/CommonSubsystems/Public/Subsystems/CommonWorldSubsystem.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Subsystems/Components/CommonTickComponent.h"
 #include "Subsystems/WorldSubsystem.h"
 
 #include "CommonWorldSubsystem.generated.h"
@@ -10,8 +11,11 @@
 	public: \
 		static bool HasInstance(const UObject* ContextObject) \
 		{ \
-			check(IsValid(ContextObject)); \
-			 \
+			if (!IsValid(ContextObject)) \
+			{ \
+				return false; \
+			} \
+			\
 			const UWorld* World = ContextObject->GetWorld(); \
 			if (!IsValid(World)) \
 			{ \
@@ -30,16 +34,40 @@
 		static ThisClass& Get(const UObject* ContextObject) \
 		{ \
 			check(IsValid(ContextObject)); \
-			 \
+			\
 			const UWorld* World = ContextObject->GetWorld(); \
 			check(IsValid(World)); \
 			\
 			auto* ThisSubsystem = World->GetSubsystem<ThisClass>(); \
 			check(IsValid(ThisSubsystem)); \
-			 \
+			\
 			auto& ThisSubsystemRef = *ThisSubsystem; \
 			return ThisSubsystemRef; \
-		} \
+		}
+
+/**
+ * @see		ENetMode
+ */
+UENUM()
+enum class ECommonNetMode : uint8
+{
+	/** Standalone: a game without networking, with one or more local players. Still considered a server because it has all server functionality. */
+	Standalone,
+
+	/** Dedicated server: server with no local players. */
+	DedicatedServer,
+
+	/** Listen server: a server that also has a local player who is hosting the game, available to other players on the network. */
+	ListenServer,
+
+	/**
+	 * Network client: client connected to a remote server.
+	 * Note that every mode less than this value is a kind of server, so checking NetMode < NM_Client is always some variety of server.
+	 */
+	Client,
+
+	MAX,
+};
 
 /**
  * World subsystem with commonly used functions.
@@ -47,8 +75,139 @@
 UCLASS(Abstract)
 class COMMONSUBSYSTEMS_API UCommonWorldSubsystem
 	: public UWorldSubsystem
+	, public FCommonTickComponent
 {
 	GENERATED_BODY()
 	// COMMON_SUBSYSTEMS_WORLD_BODY()
 	// ^^^ Include this in your override of the subsystem ^^^
+
+public:
+	UCommonWorldSubsystem();
+
+	//~UWorldSubsystem Interface
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	virtual bool DoesSupportWorldType(const EWorldType::Type WorldType) const override;
+	//~End of UWorldSubsystem Interface
+
+protected:
+	/**
+	 * Called each tick interval.
+	 * @param	DeltaSeconds time in seconds since last tick.
+	 */
+	virtual void Tick(float DeltaSeconds);
+
+public:
+	/**
+	 * Check whether a given net mode is supported.
+	 * @param	NetMode net mode to check.
+	 * @return	If true, net mode is supported, false otherwise.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	bool IsNetModeSupported(ECommonNetMode NetMode) const;
+
+	/**
+	 * Get net mode.
+	 * @param	OutNetMode output parameter. The net mode.
+	 * @param	OverrideWorld world to use. If nullptr, the one associated with this subsystem will be used instead.
+	 * @return	If true, net mode has been assigned, false otherwise.
+	 */
+	bool GetSafeNetMode(ENetMode& OutNetMode, const UWorld* OverrideWorld = nullptr) const;
+
+	/**
+	 * Get timer manager instance for this world.
+	 * @return Timer manager for this world.
+	 */
+	FTimerManager& GetTimerManager() const;
+
+protected:
+	/**
+	 * Called on world initialization
+	 */
+	virtual void OnWorldInitialized();
+
+	/**
+	 * Add given net mode in supported list.
+	 * @param	NetMode net mode to add.
+	 */
+	void AddSupportedNetMode(ECommonNetMode NetMode);
+
+	/**
+	 * Remove given net mode from supported list.
+	 * @param	NetMode net mode to remove.
+	 */
+	void RemoveSupportedNetMode(ECommonNetMode NetMode);
+
+	/**
+	 * Clear supported net modes.
+	 */
+	void ClearSupportedNetMode();
+
+	/**
+	 * Check whether this subsystem has to be create with the current net mode.
+	 * @param	World world to retrieve information from.
+	 * @return	If true, we do want to create this subsystem with the current net mode, false otherwise.
+	 */
+	bool CheckNetMode(const UWorld* World) const;
+
+	/**
+	 * Check whether this subsystem has to be created with the current level.
+	 * @param	World world to retrieve information from.
+	 * @return	If true, we do want to create this subsystem with the current level, false otherwise.
+	 */
+	bool CheckLevelName(const UWorld* World) const;
+
+private:
+	/**
+	 * Called on post world initialization.
+	 * @param	NewWorld initialized world.
+	 */
+	void PostInitWorldInternal(UWorld* NewWorld);
+
+#if WITH_EDITOR
+	/**
+	 * Called on post PIE world initialization.
+	 * @param	bSimulating true if world is simulating, false otherwise.
+	 */
+	void PostInitPieWorldInternal(bool bSimulating);
+#endif
+
+protected:
+	/** If non-empty, the subsystem will only be initialized if the level name is in this list. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Initialization")
+	TArray<FString> LevelAllowlist;
+
+	/** If non-empty, the subsystem will *NOT* be initialized if the level name is in this list. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Initialization")
+	TArray<FString> LevelBlocklist;
+
+private:
+	/** Don't initialize the subsystem unless the world matches the given Net Modes */
+	UPROPERTY(EditDefaultsOnly, Category="Initialization", meta=(Bitmask, BitmaskEnum="ECommonNetMode"))
+	uint8 InitializationNetModeMask = 0;
+
+protected:
+	/**
+	 * If true, enables in an "Untitled" level. These are usually special intermediate/transient levels created by the
+	 * engine.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Initialization", AdvancedDisplay)
+	bool bEnableInUntitledLevel = false;
+
+	/** If true, enables in the Transition Level */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Initialization", AdvancedDisplay)
+	bool bEnableInTransitionLevel = false;
+
+private:
+	/** Delegate associated with UCommonWorldSubsystem::PostInitWorldInternal(). */
+	FDelegateHandle PostInitWorldDelegateHandle;
+
+	/** If true, we've already called UCommonWorldSubsystem::PostInitWorldInternal(), false otherwise. */
+	bool bHasPostWorldInitialized = false;
+
+#if WITH_EDITOR
+	/** Delegate associated with UCommonWorldSubsystem::PostInitPieWorldInternal(). */
+	FDelegateHandle PostInitPieWorldDelegateHandle;
+#endif
 };
